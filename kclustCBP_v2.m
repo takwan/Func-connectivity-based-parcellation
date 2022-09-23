9b6c1d6c342c552b1c6d8d356d8714928e946ef5
taekwan <tkim.neurosci@gmail.com>
2022-09-23 18:11:47 +0900

    Update kclustCBP_v2.m

--- kclustCBP_v2.m
+++ kclustCBP_v2.m
@@ -1,3 +1,7 @@
+%%%% This code was a part of the "Unbalanced fronto-pallidal neurocircuit underlying set shifting in obsessive-compulsive disorder" project
+%%%% Please see details of the work in Kim, T et al. (2020) Brain. https://doi.org/10.1093/brain/awab483
+%%%% by Taekwan Kim
+
 % prep data
 grp = ['OCD'];   %HCs, OCD, or all
 if grp == 'HCs'
